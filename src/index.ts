4f7d5b66262a4b76206caf34e5fbd370ccb25850
Abhisach687 <138113508+Abhisach687@users.noreply.github.com>
2024-01-02 20:47:07 +0545

    validateUpdateTodo

--- src/index.ts
+++ src/index.ts
@@ -33,11 +33,11 @@ const validateTodo = [
 ];
 
 const validateUpdateTodo = [
-  body("title").optional().notEmpty().withMessage("Title is required"),
+  body("title").optional().notEmpty().withMessage("Title cannot be empty"),
   body("completed")
     .optional()
     .isBoolean()
-    .withMessage("Completed must be a boolean value"),
+    .withMessage("Completed must be a boolean"),
 ];
 
 const users = [
@@ -222,6 +222,10 @@ app.put(
   "/todos/:id",
   validateUpdateTodo,
   (req: express.Request, res: express.Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
     const token = req.headers.authorization?.split(" ")[1];
     if (!token) {
       return res.status(401).json({ message: "Unauthorized" });
